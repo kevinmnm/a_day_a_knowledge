f8ec78975f9f00bbc1a2948a3ebd18258516ab74
Kevin Song <kevinmnm2010@gmail.com>
2020-07-24 18:35:29 -0400

    Add auto change month value

--- ADAK.js
+++ ADAK.js
@@ -1,24 +1,49 @@
+
 new Vue({
 	el: '#calendarApp',
     data: {
     	year: 2020,
         all_2020: [],
-        JulyPrevDays: [],
-        currentValue: new Date().getMonth(),
+        daysList: [],
+        currentValue: new Date().getMonth() + 1,
         monthTxt: false
     },
-
+    methods: {
+        nextM: function(e){
+            e.stopPropagation();
+            this.currentValue++;
+            if (this.currentValue === 13){
+                this.currentValue = 1;
+            }
+        },
+        prevM: function(e){
+            e.stopPropagation();
+            this.currentValue--;
+            if (this.currentValue === 0){
+                this.currentValue = 12;
+            }
+        }
+    },
     mounted() {
     	//This creates July,2020 missing days of 1-23;
-    	for (let i=1; i<24; i++){
-        	this.JulyPrevDays.push({
-            	month: 7,
-            	day: i,
+    	// for (let i=1; i<24; i++){
+        // 	this.JulyPrevDays.push({
+        //     	month: this.currentValue,
+        //     	day: i,
+        //         topic: ''
+        //     });
+        // }
+
+        let currentMonthLastDay = moment().endOf('month').week();
+        for (let i=1; i<currentMonthLastDay+1; i++){
+            this.daysList.push({
+                month: this.currentValue,
+                day: i,
                 topic: ''
             });
         }
         
-        //This creates all months in 2020;
+        //This creates all months in months section for 2020;
         for (let i=0; i<12; i++){
         	this.all_2020.push({
             	yy: 2020,
@@ -30,3 +55,8 @@ new Vue({
         }
     }
 });
+
+//alert(moment().max('2020'));
+//alert(moment().min('2020'));
+alert(moment().endOf('month').week());
+alert(moment().month());
