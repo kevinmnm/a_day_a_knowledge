ea0c716a9ed226c199afee9d80e6f06ad679fcf9
Kevin Song <kevinmnm2010@gmail.com>
2020-07-24 17:03:18 -0400

    Fixed CSS and add month changing function

--- ADAK.js
+++ ADAK.js
@@ -1,22 +1,32 @@
 new Vue({
 	el: '#calendarApp',
     data: {
-    	year: new Date().getFullYear(),
-        month: new Date().getMonth() + 1,
-        prevDays: []
+    	year: 2020,
+        all_2020: [],
+        JulyPrevDays: [],
+        currentValue: new Date().getMonth(),
+        monthTxt: false
     },
-    methods: {
-    	
-    },
+
     mounted() {
-    	//This creates July,2020 days from 1-23;
+    	//This creates July,2020 missing days of 1-23;
     	for (let i=1; i<24; i++){
-        	this.prevDays.push({
+        	this.JulyPrevDays.push({
+            	month: 7,
             	day: i,
                 topic: ''
             });
         }
-        console.log(this.prevDays);
         
+        //This creates all months in 2020;
+        for (let i=0; i<12; i++){
+        	this.all_2020.push({
+            	yy: 2020,
+                mm: i + 1,
+                mmt: function(){
+                	return moment.months()[i];
+                }
+            });
+        }
     }
 });
