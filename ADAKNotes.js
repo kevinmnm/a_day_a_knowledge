91b90cd3066dfd333e81bc279b26254311fd1eb3
Kevin Song <kevinmnm2010@gmail.com>
2020-07-29 18:47:33 -0400

    Add image

--- ADAKNotes.js
+++ ADAKNotes.js
@@ -120,7 +120,7 @@ let AKAD = [
             `,
         referenceLink1: 'https://vuejs.org/v2/guide/events.html#Event-Modifiers',
         referenceLink2: 'https://www.digitalocean.com/community/tutorials/vuejs-events',
-        codepenEmbed: `No codepen.io example available for this content.`
+        codepenEmbed: `No codepen.io example available for this content. <br><br><img src='img/event_modifiers.png'></img>`
     },
     {
         uniqueIdMatch: 'z710',
