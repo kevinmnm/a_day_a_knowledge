a39c1166d95054daae2eca3b10e9a02ee75063f1
Kevin Song <kevinmnm2010@gmail.com>
2020-08-02 19:35:03 -0400

    Add reference link 2 for content z721

--- ADAKNotes.js
+++ ADAKNotes.js
@@ -263,7 +263,7 @@ let AKAD = [
         topic: 'Complicated Component Usage & Array Iterations',
         description: 'PLEASE LOOK OVER THIS CONTENT MULTIPLE TIME TO GET COMFORTABLE. This content took whole day for me write. Complicated Vue component usage was utilized. Some JavaScript arry iterations are noted as well.',
         referenceLink1: 'https://www.w3schools.com/js/js_array_iteration.asp',
-        referenceLink2: '',
+        referenceLink2: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach',
         codepenEmbed: `<iframe height="600" style="width: 100%;" scrolling="no" title="Advanced Component Usage &amp; Array Iterations" src="https://codepen.io/kevinmnm/embed/rNeBVKq?height=265&theme-id=dark&default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
         See the Pen <a href='https://codepen.io/kevinmnm/pen/rNeBVKq'>Advanced Component Usage &amp; Array Iterations</a> by kevin
         (<a href='https://codepen.io/kevinmnm'>@kevinmnm</a>) on <a href='https://codepen.io'>CodePen</a>.
