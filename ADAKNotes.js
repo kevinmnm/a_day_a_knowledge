9f2068766ac8b7d38681d32803a95b3634090678
Kevin Song <kevinmnm2010@gmail.com>
2020-08-04 15:41:59 -0400

    Rest paramenter

--- ADAKNotes.js
+++ ADAKNotes.js
@@ -312,7 +312,7 @@ let AKAD = [
         topic: 'Rest Parameter (...args)',
         description: 'When having long list of array, rest parameter will come in very handy.',
         referenceLink1: 'https://www.geeksforgeeks.org/javascript-rest-operator/',
-        referenceLink2: '',
+        referenceLink2: 'https://www.javascripttutorial.net/es6/javascript-rest-parameters/',
         codepenEmbed: ``
     },
     {
