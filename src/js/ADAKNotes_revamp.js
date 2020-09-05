675aeaf6b9c1362e1ebc300bf55912f08d4f10a3
Kevin Song <kevinmnm2010@gmail.com>
2020-09-05 01:45:23 -0400

    add deconstructuring assignment content

--- src/js/ADAKNotes_revamp.js
+++ src/js/ADAKNotes_revamp.js
@@ -661,14 +661,17 @@ let AKAD = [
     </iframe>`
     },
     {
-      uniqueIdMatch: '',
-      date: '',
-      content: '',
-      topic: '',
-      description: '',
-      referenceLink1: '',
-      referenceLink2: '',
-      codepenEmbed: ``
+      uniqueIdMatch: 'z822',
+      date: '08/22/2020',
+      content: 'JS',
+      topic: 'Deconstructuring Assignment',
+      description: 'Learning basics before scrutinizing ES6 Deconstructuring.',
+      referenceLink1: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment',
+      referenceLink2: 'https://www.youtube.com/watch?v=-vR3a11Wzt0&ab_channel=freeCodeCamp.org',
+      codepenEmbed: `<iframe height="800" style="width: 100%;" scrolling="no" title="Deconstructuring Assignment" src="https://codepen.io/kevinmnm/embed/mdPqZmX?height=265&theme-id=dark&default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+      See the Pen <a href='https://codepen.io/kevinmnm/pen/mdPqZmX'>Deconstructuring Assignment</a> by kevin
+      (<a href='https://codepen.io/kevinmnm'>@kevinmnm</a>) on <a href='https://codepen.io'>CodePen</a>.
+    </iframe>`
     }
     
 ];
