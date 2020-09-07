8fd5ba8ebe1a90352b9ee48ac38a012a2431c177
Kevin Song <kevinmnm2010@gmail.com>
2020-09-06 23:43:02 -0400

    add new content

--- src/js/ADAKNotes_revamp.js
+++ src/js/ADAKNotes_revamp.js
@@ -685,6 +685,19 @@ let AKAD = [
       See the Pen <a href='https://codepen.io/kevinmnm/pen/GRZyQEZ'>Destructuring Object</a> by kevin
       (<a href='https://codepen.io/kevinmnm'>@kevinmnm</a>) on <a href='https://codepen.io'>CodePen</a>.
     </iframe>`
+    },
+    {
+      uniqueIdMatch: 'z824',
+      date: '08/24/2020',
+      content: 'React',
+      topic: 'React Basic Syntax',
+      description: 'Be aware that Babel must be used to compile JSX format.',
+      referenceLink1: '',
+      referenceLink2: '',
+      codepenEmbed: `<iframe height="650" style="width: 100%;" scrolling="no" title="React Basic Syntax" src="https://codepen.io/kevinmnm/embed/GRZyGjp?height=265&theme-id=dark&default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+      See the Pen <a href='https://codepen.io/kevinmnm/pen/GRZyGjp'>React Basic Syntax</a> by kevin
+      (<a href='https://codepen.io/kevinmnm'>@kevinmnm</a>) on <a href='https://codepen.io'>CodePen</a>.
+    </iframe>`
     }
     
 ];
