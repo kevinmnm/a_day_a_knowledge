f5f5a8a58006445686a201919a82845fb69d2f61
Kevin Song <kevinmnm2010@gmail.com>
2020-09-15 12:58:23 -0400

    add content

--- src/js/ADAKNotes_revamp.js
+++ src/js/ADAKNotes_revamp.js
@@ -893,6 +893,19 @@ let AKAD = [
       See the Pen <a href='https://codepen.io/kevinmnm/pen/QWNxBQg'>React useEffect() In-Depth</a> by kevin
       (<a href='https://codepen.io/kevinmnm'>@kevinmnm</a>) on <a href='https://codepen.io'>CodePen</a>.
     </iframe>`
+    },
+    {
+      uniqueIdMatch: 'z99',
+      date: '09/09/2020',
+      content: 'React',
+      topic: 'Ref in Class-Component',
+      description: 'Use Ref to select element instead of selecting DOM.',
+      referenceLink1: 'https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/13556338#content',
+      referenceLink2: 'https://reactjs.org/docs/refs-and-the-dom.html',
+      codepenEmbed: `<iframe height="750" style="width: 100%;" scrolling="no" title="React Ref in Class-Component" src="https://codepen.io/kevinmnm/embed/NWNBpbX?height=265&theme-id=dark&default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+      See the Pen <a href='https://codepen.io/kevinmnm/pen/NWNBpbX'>React Ref in Class-Component</a> by kevin
+      (<a href='https://codepen.io/kevinmnm'>@kevinmnm</a>) on <a href='https://codepen.io'>CodePen</a>.
+    </iframe>`
     }
     
 ];
