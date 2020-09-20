2c78b32fcef0a6c603c41cf9936f9bf1331425c0
Kevin Song <kevinmnm2010@gmail.com>
2020-09-20 19:02:07 -0400

    add void operator encounter content

--- src/js/ADAKNotes_revamp.js
+++ src/js/ADAKNotes_revamp.js
@@ -1049,6 +1049,19 @@ let AKAD = [
       See the Pen <a href='https://codepen.io/kevinmnm/pen/oNxaWez'>Dispatch Input Value</a> by kevin
       (<a href='https://codepen.io/kevinmnm'>@kevinmnm</a>) on <a href='https://codepen.io'>CodePen</a>.
     </iframe>`
+    },
+    {
+      uniqueIdMatch: 'z921',
+      date: '09/21/2020',
+      content: 'JS',
+      topic: 'void Operator Encounter',
+      description: 'void operator just resolved animation trigger issue.',
+      referenceLink1: 'https://css-tricks.com/restart-css-animation/',
+      referenceLink2: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/void',
+      codepenEmbed: `<iframe height="1000" style="width: 100%;" scrolling="no" title="void Operator Encounter" src="https://codepen.io/kevinmnm/embed/wvGQMGa?height=265&theme-id=dark&default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+      See the Pen <a href='https://codepen.io/kevinmnm/pen/wvGQMGa'>void Operator Encounter</a> by kevin
+      (<a href='https://codepen.io/kevinmnm'>@kevinmnm</a>) on <a href='https://codepen.io'>CodePen</a>.
+    </iframe>`
     }
     
 ];
