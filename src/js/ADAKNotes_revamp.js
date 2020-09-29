34a5bf053d065e23e69958c29a25112ed1270b2e
Kevin Song <kevinmnm2010@gmail.com>
2020-09-29 13:49:26 -0400

    added new content for express.js

--- src/js/ADAKNotes_revamp.js
+++ src/js/ADAKNotes_revamp.js
@@ -1251,6 +1251,19 @@ let AKAD = [
         See the Pen <a href='https://codepen.io/kevinmnm/pen/mdPYEor'>Express.js Simple Routing</a> by kevin
         (<a href='https://codepen.io/kevinmnm'>@kevinmnm</a>) on <a href='https://codepen.io'>CodePen</a>.
       </iframe>`
+    },
+    {
+        uniqueIdMatch: 'z107',
+        date: '10/07/2020',
+        content: 'Other',
+        topic: 'use(), get(), post()',
+        description: 'Different routing methods usage in Express.js',
+        referenceLink1: '',
+        referenceLink2: '',
+        codepenEmbed: `<iframe height="1000" style="width: 100%;" scrolling="no" title="Express.js use(), get(), post()" src="https://codepen.io/kevinmnm/embed/xxVNRrX?height=265&theme-id=dark&default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+        See the Pen <a href='https://codepen.io/kevinmnm/pen/xxVNRrX'>Express.js use(), get(), post()</a> by kevin
+        (<a href='https://codepen.io/kevinmnm'>@kevinmnm</a>) on <a href='https://codepen.io'>CodePen</a>.
+      </iframe>`
     }
     
 ];
