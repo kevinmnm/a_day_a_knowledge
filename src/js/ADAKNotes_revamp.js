c1b1ccb069b468021108089af957d9288bf30fba
Kevin Song <kevinmnm2010@gmail.com>
2020-09-29 14:08:53 -0400

    added body-parser content

--- src/js/ADAKNotes_revamp.js
+++ src/js/ADAKNotes_revamp.js
@@ -1264,6 +1264,19 @@ let AKAD = [
         See the Pen <a href='https://codepen.io/kevinmnm/pen/xxVNRrX'>Express.js use(), get(), post()</a> by kevin
         (<a href='https://codepen.io/kevinmnm'>@kevinmnm</a>) on <a href='https://codepen.io'>CodePen</a>.
       </iframe>`
+    },
+    {
+        uniqueIdMatch: 'z108',
+        date: '10/08/2020',
+        content: 'Other',
+        topic: 'body-parser',
+        description: 'body-parser is one of the eseential package to include in Express.js projects.',
+        referenceLink1: '',
+        referenceLink2: '',
+        codepenEmbed: `<iframe height="1000" style="width: 100%;" scrolling="no" title="Express.js body-parser" src="https://codepen.io/kevinmnm/embed/JjXqbev?height=265&theme-id=dark&default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+        See the Pen <a href='https://codepen.io/kevinmnm/pen/JjXqbev'>Express.js body-parser</a> by kevin
+        (<a href='https://codepen.io/kevinmnm'>@kevinmnm</a>) on <a href='https://codepen.io'>CodePen</a>.
+      </iframe>`
     }
     
 ];
